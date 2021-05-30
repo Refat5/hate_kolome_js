947dcc49ca04b44c726693099a47baef161ffe23
AR Refat <mdrefat382139@gmail.com>
2021-05-30 23:39:23 +0600

    js basic first commit

---
+++ script.js
@@ -0,0 +1,58 @@
+if (4 < 5) {
+    console.log('this statement is true ');
+}
+
+//statement true
+var myName = 'refat';
+if (myName == 'refat') {
+    console.log('this is my name');
+}
+
+// statement false
+
+var myRoll = 07;
+
+if (myRoll == 02) {
+    console.log('this is my roll');
+}
+
+// more condition
+
+var myAge = 23;
+if (32 >= 32) {
+    console.log('you are children');
+}
+
+if (myAge > 3) {
+    console.log('this is my age');
+}
+
+if (myAge <= 332) {
+    console.log('i am a adult person ');
+}
+
+if (myAge > 3 && myAge < 35) {
+    console.log('you are a young person');
+}
+
+/// if else condition
+
+if (myAge > 32) {
+    console.log('No you are not aged enough to be a adult ');
+} else {
+    console.log('Now you are Adult person');
+}
+
+// else if condition
+
+var Date = 'friday';
+
+if (Date === 'sunday') {
+    console.log('It is Partially closed');
+} else if (Date === 'thursday') {
+    console.log('It is Working Day');
+} else if (Date === 'saturday') {
+    console.log('Some office are closed');
+} else {
+    console.log('It is Closed today');
+}
